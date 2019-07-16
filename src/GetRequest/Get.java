87e0f1665ccb45de7c2f71f6d69066afff79b5ed
shay123g <49873692+shay123g@users.noreply.github.com>
2019-07-16 20:09:56 +0300

    +

---
+++ src/GetRequest/Get.java
@@ -0,0 +1,17 @@
+package GetRequest;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class Get
+{
+}
